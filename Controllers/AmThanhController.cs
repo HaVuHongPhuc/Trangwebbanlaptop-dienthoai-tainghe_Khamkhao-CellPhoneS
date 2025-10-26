643c9337052b932b4a342fb6e83c9e6b2673629e
HaVuHongPhuc <havuhongphuc1426@gmail.com>
2025-10-26 16:09:49 +0700

    first commit

---
+++ Controllers/AmThanhController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AmThanhController : Controller
+    {
+        // GET: AmThanh
+        public ActionResult AmThanh()
+        {
+            return View();
+        }
+        public ActionResult AirPods()
+        {
+            return View();
+        }
+        public ActionResult ap1()
+        {
+            return View();
+        }
+        public ActionResult ap2()
+        {
+            return View();
+        }
+        public ActionResult ap3()
+        {
+            return View();
+        }
+        public ActionResult Huawei()
+        {
+            return View();
+        }
+        public ActionResult Hua1()
+        {
+            return View();
+        }
+        public ActionResult Hua2()
+        {
+            return View();
+        }
+        public ActionResult Hua3()
+        {
+            return View();
+        }
+        public ActionResult Hua4()
+        {
+            return View();
+        }
+        public ActionResult Hua5()
+        {
+            return View();
+        }
+        public ActionResult Hua6()
+        {
+            return View();
+        }
+        public ActionResult Samsung()
+        {
+            return View();
+        }
+        public ActionResult ss1()
+        {
+            return View();
+        }
+        public ActionResult ss2()
+        {
+            return View();
+        }
+        public ActionResult ss3()
+        {
+            return View();
+        }
+        public ActionResult ss4()
+        {
+            return View();
+        }
+        public ActionResult ss5()
+        {
+            return View();
+        }
+        public ActionResult ss6()
+        {
+            return View();
+        }
+        public ActionResult Sony()
+        {
+            return View();
+        }
+        public ActionResult sn1()
+        {
+            return View();
+        }
+        public ActionResult sn2()
+        {
+            return View();
+        }
+        public ActionResult sn3()
+        {
+            return View();
+        }
+        public ActionResult sn4()
+        {
+            return View();
+        }
+        public ActionResult sn5()
+        {
+            return View();
+        }
+        public ActionResult sn6()
+        {
+            return View();
+        }
+        public ActionResult Xiaomi()
+        {
+            return View();
+        }
+        public ActionResult Xia1()
+        {
+            return View();
+        }
+        public ActionResult Xia2()
+        {
+            return View();
+        }
+        public ActionResult Xia3()
+        {
+            return View();
+        }
+        public ActionResult Xia4()
+        {
+            return View();
+        }
+        public ActionResult Xia5()
+        {
+            return View();
+        }
+        public ActionResult Xia6()
+        {
+            return View();
+        }
+    }
+}
